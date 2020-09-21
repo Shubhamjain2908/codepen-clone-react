97f332d443e678c75f8665ec80f215d5aa2c0e92
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-22 00:01:06 +0530

    install codemirror

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Editor from './Editor';
 
 function App() {
   return (
-    <div>Hello world!</div>
+    <>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
