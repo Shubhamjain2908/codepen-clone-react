d3c04fd3cfb1ba0a52fd7d2d6edc1f6be301233e
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-22 00:39:47 +0530

    Updates

---
+++ precache-manifest.2d44d4b5ec838434c9bd70ab2793100c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e6bea9708c9f24f5704abfbc3adc93",
+    "url": "/codepen-clone-react/index.html"
+  },
+  {
+    "revision": "11b459cabba4768d8e62",
+    "url": "/codepen-clone-react/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "78632fb8a72a38d60170",
+    "url": "/codepen-clone-react/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "11b459cabba4768d8e62",
+    "url": "/codepen-clone-react/static/js/2.ca6d66a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone-react/static/js/2.ca6d66a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78632fb8a72a38d60170",
+    "url": "/codepen-clone-react/static/js/main.72912982.chunk.js"
+  },
+  {
+    "revision": "9450e997428911c7b8f7",
+    "url": "/codepen-clone-react/static/js/runtime-main.80cf1028.js"
+  }
+]);
