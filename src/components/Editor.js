31b5771045329fb95e003459c53eb05a6ac7aa05
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-22 00:30:53 +0530

    open close button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,23 +1,33 @@
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/theme/material.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Controlled as ControllerEditor } from 'react-codemirror2';
 
 function Editor(props) {
     const { language, displayName, value, onChange } = props;
+
+    const [open, setOpen] = useState(true);
 
     function handleChange(editor, data, value) {
         onChange(value);
     }
 
     return (
-        <div className="editor-container" >
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button>O/C</button>
+                <button
+                    type="button"
+                    className="expand-collapse-btn"
+                    onClick={() => setOpen(prevOpen => !prevOpen)}
+                >
+                    <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+                </button>
             </div>
             <ControllerEditor
                 onBeforeChange={handleChange}
